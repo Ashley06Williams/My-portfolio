21fcf744732f756c87f33d9c963f1b89526421a8
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-04 22:40:52 +0200

    added header to hero section with styling
    Requires navigation as well as a few design animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import HeaderSection from "@/components/header-section";
+import HeroSection from "@/components/hero-section";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-50 bg-blueGrey h-screen">
-      <main className=""></main>;
+    <div className="bg-blueGrey">
+      <div className=" h-screen max-w-[1200px] mx-auto">
+        <main className="">
+          <HeaderSection />
+          <HeroSection />
+        </main>
+      </div>
     </div>
   );
 }
