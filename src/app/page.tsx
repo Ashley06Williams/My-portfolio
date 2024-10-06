b08b29c058a27e2ed217d3690850bee790d9612d
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-06 20:33:48 +0200

    Added my abilities section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import AbilitySection from "@/components/ability-section";
 import HeaderSection from "@/components/header-section";
 import HeroSection from "@/components/hero-section";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-blueGrey">
-      <div className=" h-screen max-w-[1200px] mx-auto">
+    <div className="bg-blueGrey h-full">
+      <div className=" max-w-[1200px] mx-auto">
         <main className="">
           <HeaderSection />
           <HeroSection />
+          <AbilitySection />
         </main>
       </div>
     </div>
