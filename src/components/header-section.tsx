a7ff331ec6b726a155e607c27ced2b98a242b9e4
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-23 19:11:17 +0200

    feedBack/Navbar spans whole page

--- src/components/header-section.tsx
+++ src/components/header-section.tsx
@@ -34,42 +34,44 @@ export default function HeaderSection() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <nav className="w-full px-4 max-w-[1200px]  fixed backdrop-blur-[11px] bg-blueGrey-dark bg-opacity-20 flex flex-wrap justify-between md:space-x-0 mx-auto pt-4  items-center  pb-4 ">
-      <a
-        href="/"
-        onClick={handleHomeClick}
-        className="text-white font-extrabold mr-8 md:mr-0 "
-      >
-        CODEDBY<span className="text-red">ASH</span>
-      </a>
-      <div className="MOBILE-MENU block md:hidden ">
-        {!navOpen ? (
-          <button
-            onClick={() => setNavOpen(true)}
-            className="flex items-center  px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red hover:border-red"
-          >
-            <Bars3Icon className="w-5 h-5" />
-          </button>
-        ) : (
-          <button
-            onClick={() => setNavOpen(false)}
-            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red hover:border-red"
-          >
-            <XMarkIcon className="w-5 h-5" />
-          </button>
-        )}
-      </div>
+    <nav className="w-full px-4  fixed backdrop-blur-[11px] bg-blueGrey-dark bg-opacity-35 flex flex-wrap justify-center md:space-x-0 mx-auto pt-4  items-center pb-4 ">
+      <div className="flex items-center justify-between w-[1200px]">
+        <a
+          href="/"
+          onClick={handleHomeClick}
+          className="text-white font-extrabold mr-8 md:mr-0 "
+        >
+          CODEDBY<span className="text-red">ASH</span>
+        </a>
+        <div className="MOBILE-MENU block md:hidden ">
+          {!navOpen ? (
+            <button
+              onClick={() => setNavOpen(true)}
+              className="flex items-center  px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red hover:border-red"
+            >
+              <Bars3Icon className="w-5 h-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-red hover:border-red"
+            >
+              <XMarkIcon className="w-5 h-5" />
+            </button>
+          )}
+        </div>
 
-      <div className="MENU space-x-4 md:space-x-6 hidden md:block">
-        <ul className="flex flex-col md:flex-row p-4 sm:p-0 ">
-          {menuItems.map((menuItem) => (
-            <li key={menuItem.id}>
-              <NavLink href={menuItem.slug} title={menuItem.name} />
-            </li>
-          ))}
-        </ul>
+        <div className="MENU space-x-4 md:space-x-6 hidden md:block">
+          <ul className="flex flex-col md:flex-row p-4 sm:p-0 ">
+            {menuItems.map((menuItem) => (
+              <li key={menuItem.id}>
+                <NavLink href={menuItem.slug} title={menuItem.name} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        {navOpen ? <MenuOverlay links={menuItems} /> : null}
       </div>
-      {navOpen ? <MenuOverlay links={menuItems} /> : null}
     </nav>
   );
 }
