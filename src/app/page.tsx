17fa8285dc2e4e7b639fec37abfe4d5df46e4d36
Ashley06Williams <ashley@waterfalldigital.co.za>
2024-10-24 12:37:16 +0200

    fixed overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ import { useState } from "react";
 export default function Home() {
   const [modal, setModal] = useState(false);
   return (
-    <div>
-      <header className="w-full flex justify-center items-start">
+    <div className="overflow-x-hidden">
+      <header className="w-full flex justify-center items-start ">
         <HeaderSection />
       </header>
       <div className="bg-blueGrey h-full flex flex-col items-center ">
-        <div className="max-w-[1250px] mx-auto ">
+        <div className="max-w-[1200px] mx-auto ">
           <main className="mx-8">
             <HeroSection setModal={setModal} modal={modal} />
             <AbilitySection />
--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -2,17 +2,19 @@ import React from "react";
 
 export default function FooterSection() {
   return (
-    <div className="mx-[250px] flex justify-between">
-      <div>
-        <p className="text-[20px] p-4 text-white font-bold mt-2">
+    <div className=" flex justify-between p-2  h-full md:mx-[200px] mx-0">
+      <div className="">
+        <p className="text-[20px] p-3 text-white font-bold mt-1">
           CODEDBY<span className="text-red">ASH</span>
         </p>
-        <p className="px-4 text-slate-300">
+        <p className="px-2 text-slate-300 text-[10px]">
           Copyright &copy; 2024 Ashley Williams. All Rights Reserved.
         </p>
-        <p className="px-4  text-slate-300">Made with ❤️ and NextJS</p>
+        <p className="px-2  text-slate-300 text-[10px] ">
+          Made with ❤️ and NextJS
+        </p>
       </div>
-      <div className="flex px-6 space-x-8">
+      <div className="flex px-6 space-x-2">
         <img src="/gitHub.svg" className="w-10" />
         <img src="/linkedIn.svg" alt="" className="w-10" />
       </div>
