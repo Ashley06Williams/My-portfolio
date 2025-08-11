e78efe03f79f16d19d2732222c720315de3496b1
CodedByAsh <codedbyash@gmail.com>
2025-08-11 10:55:11 +0200

    Imporved hero section design, added profile photo

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
 
 type PopupProps = {
   modal: boolean;
@@ -10,9 +11,10 @@ type PopupProps = {
 
 export default function HeroSection({ setModal, modal }: PopupProps) {
   return (
-    <div className="bg-blueGrey md:max-w-[1200px] mx-auto section-spacing h-screen flex flex-col flex-wrap justify-center items-start p-4 text-left ">
+    <div className="bg-blueGrey md:max-w-[1200px] mx-auto section-spacing h-screen grid grid-cols-3  justify-center items-center p-5 text-left ">
       {/* <div className="max-w-[800px] "> */}
-      <h2 className="text-red text-[20px] md:text-[40px]">Hello, I&apos;m</h2>
+      <div className="col-span-2">
+      <p className="text-red text-[20px] md:text-[30px] mb-[-10px]">Hello, I&apos;m</p>
       <TypeAnimation
         sequence={[
           // Same substring at the start will only be typed out once, initially
@@ -24,16 +26,14 @@ export default function HeroSection({ setModal, modal }: PopupProps) {
         wrapper="span"
         speed={25}
         repeat={1}
-        className="text-white font-bold text-[45px]  md:text-[50px] lg:text-[70px] leading-tight md:leading-normal"
+        className="text-white font-bold text-[45px]  md:text-[40px] lg:text-[60px] leading-tight md:leading-normal"
       />
-      <p className="text-[13px] md:text-[18px] max-w-[550px] text-white mb-12 pr-2 md:pr-0">
-        An aspiring web developer with a passion for learning and building
-        modern, responsive websites. I am constantly seeking new opportunities
-        to improve my skills.
+      <p className="text-[13px] md:text-[18px] max-w-[600px] text-white mb-8 pr-2 md:pr-0">
+         A self-taught web developer with a passion for building custom applications currently using React and Next.JS
       </p>
       <div className="flex flex-col md:flex-row gap-y-6 md:space-x-8">
         <button
-          className="bg-red p-2 md:p-4 px-14 md:px-16 text-white hover:bg-red-light rounded-tr-xl rounded-bl-xl text-[14px] md:text-[18px] font-semibold"
+          className="bg-red  text-white hover:bg-red-light rounded-tr-xl rounded-bl-xl text-xl py-3 px-12"
           onClick={() => setModal(!modal)}
         >
           Contact me
@@ -50,16 +50,19 @@ export default function HeroSection({ setModal, modal }: PopupProps) {
             a.download = "/MyResume.pdf";
             a.click();
           }}
-          className="bg-slate-100 text-red hover:bg-slate-300 p-2 md:p-4 px-14 md:px-16 rounded-tl-xl rounded-br-xl text-[14px] md:text-[18px] font-semibold"
+          className="bg-slate-100 text-red hover:bg-slate-300 rounded-tl-xl rounded-br-xl    text-xl py-3 px-12"
         >
           My Resume
         </button>
       </div>
+      </div>
+
+      <div >
+        <Image src="/ashley.png" alt="My Image" width={400} height={400} />
+      </div>
     </div>
+
     // </div>
   );
 }
 
-// <h1 className="text-white text-[80px] font-semibold text-left -mt-4 mb-6">
-//   A Frontend Developer
-// </h1>;
