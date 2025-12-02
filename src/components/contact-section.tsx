9d0b44a7c7b1c51da2ef613d177652124b4fc8a7
CodedByAsh <codedbyash@gmail.com>
2025-12-02 09:56:32 +0200

    fix: update text formatting for consistency in contact section

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -69,14 +69,14 @@ export default function FormPage({ modal, setModal }: PopupProps) {
       </div>
 
       <h1 className="text-4xl font-semibold text-white"><span className="text-red">Ready</span> to grow,<span className="text-red"> Ready</span> to learn</h1>
-      <p className="text-white text-center max-w-2xl mt-5 text-xl">If you know someone who’s looking for a motivated junior developer, or if you’re hiring yourself, I’d be excited to chat.</p>
+      <p className="text-white text-center max-w-2xl mt-5 text-xl">If you know someone who&apos;s looking for a motivated junior developer, or if you&apos;re hiring yourself, I&apos;d be excited to chat.</p>
           <button onClick={toggleModal} className="relative flex items-center bg-red border-2 border-red text-white font-medium mt-8 text-md md:text-xl group hover:bg-white p-1 rounded-full overflow-hidden">
     <div className="relative z-10 bg-white rounded-full w-[40px] h-[40px] md:w-[40px] md:h-[40px] flex items-center justify-center">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-6 w-6 md:w-8 md:h-8 text-red">
         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
       </svg>
     </div>
-    <p className="relative z-10 pl-3 pr-4 group-hover:text-red">Let's Get in Touch</p>
+    <p className="relative z-10 pl-3 pr-4 group-hover:text-red">Let&apos;s Get in Touch</p>
   </button>
 
   <Popup modal={modal} setModal={setModal} />
